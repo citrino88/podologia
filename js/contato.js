c54fe4fb18321b32fb79bd4bf3577bc68586de09
citrino88 <almeida.citrino@gmail.com>
2024-05-23 18:55:23 -0300

    retirando  codigos de menu ham errados

--- js/contato.js
+++ js/contato.js
@@ -121,11 +121,3 @@ botaoBuscar.addEventListener("click", async function(event){
       });
     }
     formulario.addEventListener("submit", handleSubmit) //trocamos para nosso const formulario
-
-    //hambuguer
-    const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
